1d8825d04a746f87a299a83ce8e592706e19a921
Adolfo Rodrigues <adolfo.rodrigues@cnj.jus.br>
2025-04-18 17:55:53 -0300

    organizando

--- iniciando/desafio-comprehesion.py
+++ iniciando/desafio-comprehesion.py
@@ -4,7 +4,7 @@ from pprint import pprint
 sorteios = ['sorteios1', 'sorteios2', 'sorteios3']
 participantes = ['Adolfo', 'Cristian', 'Jorge', 'Lucas', 'Marcos', 'Pedro', 'Rafael', 'Thiago', 'Vinicius']
 
-#pprint({sorteio: random.choice(participantes) for sorteio in sorteios})
+pprint({sorteio: random.choice(participantes) for sorteio in sorteios})
 
 grupos = ['Grupo1', 'Grupo2', 'Grupo3']
 pprint({grupo: [random.randint(1, 100) for _ in range(3)] for grupo in grupos})
